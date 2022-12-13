f12c1bd42038176c809a6f138118a61c2b5aaead
janekigenyi <janekigenyi@gmail.com>
2022-12-13 09:20:14 +0300

    completed

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,12 @@
-export function manageFriends(state, action) {
+
+export function manageFriends(state = {friends: []}, action) {
   // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {...state, friends:[...state.friends, action.payload]}
+    case "friends/remove":
+      let newFriends = [...state.friends]
+      return {...state, friends: newFriends.splice(action.payload+1, 1)}
+    default : return state
+  }
 }
